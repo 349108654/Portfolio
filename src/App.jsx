ca28092ef94808924dd5710a826122184e37ae96
Ellie Liang <e32liang@uwaterloo.ca>
2025-05-07 09:08:52 -0400

    partly finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import LogoSection from "./sections/LogoSection"
+import NavBar from "./components/NavBar"
+import FeatureCards from "./sections/FeatureCards"
+import Hero from "./sections/hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
+import ExperienceSection from "./sections/ExperienceSection"
+import TechStack from "./sections/TechStack"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
+
 const App = () => {
     return(
-        <main>
-            <div>
-                <h1>3D Portfolio!</h1>
-            </div>
-        </main>
+        <>
+            <NavBar />
+            <Hero />
+            <ShowcaseSection />
+            <LogoSection />
+            <FeatureCards /> 
+            <ExperienceSection />
+            <TechStack />
+            <Contact />
+            <Footer />
+        </>
     )
 }
 
