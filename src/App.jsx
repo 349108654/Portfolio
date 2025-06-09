d3db7cbb1022dd52e9be0ae63f1a30f6efff07f3
Ellie Liang <e32liang@uwaterloo.ca>
2025-06-09 11:56:05 -0400

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import LogoSection from "./sections/LogoSection"
-// import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar"
 // import FeatureCards from "./sections/FeatureCards"
 // import Hero from "./sections/hero"
 // import ShowcaseSection from "./sections/ShowcaseSection"
@@ -8,55 +8,20 @@
 // import Contact from "./sections/Contact"
 // import Footer from "./sections/Footer"
 
-// const App = () => {
-//     return(
-//         <>
-//             <NavBar />
-//             <Hero />
-//             <ShowcaseSection />
-//             {/* <LogoSection /> */}
-//             {/* <FeatureCards />  */}
-//             <ExperienceSection />
-//             <TechStack />
-//             <Contact />
-//             <Footer />
-//         </>
-//     )
-// }
-
-// export default App
-import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        {/* <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+    return(
+        <>
+            <NavBar />
+            {/* <Hero />
+            <ShowcaseSection /> */}
+            {/* <LogoSection /> */}
+            {/* <FeatureCards />  */}
+            {/* <ExperienceSection />
+            <TechStack />
+            <Contact />
+            <Footer /> */}
+        </>
+    )
 }
 
 export default App
