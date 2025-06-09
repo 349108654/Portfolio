463a0a2af7762d6b887d1ea96ee3e9c9c1a21afa
Ellie Liang <e32liang@uwaterloo.ca>
2025-06-09 12:13:02 -0400

    deploy config

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,7 +1,7 @@
-import AnimatedCounter from "../components/AnimatedCounter"
+// import AnimatedCounter from "../components/AnimatedCounter"
 import Button from "../components/button"
 import HeroExperience from "../components/HeroModels/HeroExperience"
-import { words } from "../constants"
+// import { words } from "../constants"
 import { useGSAP } from "@gsap/react"
 import gsap from 'gsap'
 
