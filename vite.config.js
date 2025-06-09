3a7170885d767e007e5d7444a244e0a08f177d6a
Ellie Liang <e32liang@uwaterloo.ca>
2025-06-09 12:05:26 -0400

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
   base: "/portfolio",
-}))
+})
