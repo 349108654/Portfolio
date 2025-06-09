fa674c9ea56a176325a1ec523a1e5e8bd73925b1
Ellie Liang <e32liang@uwaterloo.ca>
2025-06-09 12:43:56 -0400

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import NavBar from "./components/NavBar"
 // import Hero from "./sections/hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 import ExperienceSection from "./sections/ExperienceSection"
-import TechStack from "./sections/TechStack"
+// import TechStack from "./sections/TechStack"
 // import Contact from "./sections/Contact"
-import Footer from "./sections/Footer"
+// import Footer from "./sections/Footer"
 
 const App = () => {
     return(
@@ -17,9 +17,9 @@ const App = () => {
             {/* <LogoSection /> */}
             {/* <FeatureCards />  */}
             <ExperienceSection />
-            <TechStack />
+            {/* <TechStack /> */}
             {/* <Contact /> */}
-            <Footer />
+            {/* <Footer /> */}
         </>
     )
 }
